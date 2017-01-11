1544cec5d5c05e62968c9e302ad4116bc89d10f2
sGambolati <sebbastian.gambolati@southworks.com>
2017-01-11 09:22:25 -0300

    Add project files.

---
+++ Bot Application1/Controllers/MessagesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+using Bot_Application1.Dialogs;
+
+namespace Bot_Application1
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            //activity.Type = ActivityTypes.ContactRelationUpdate;
+
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new RootLuisDialog());
+            }
+            else
+            {
+                var reply = HandleSystemMessage(activity);
+                if (reply != null)
+                {
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+                System.Diagnostics.Debug.WriteLine(message.From);
+                System.Diagnostics.Debug.WriteLine(message.Action);
+
+                System.Diagnostics.Debug.WriteLine($"{message.From.Name} has {message.Action} you (bot).");
+                if (message.Action == "add")
+                {
+                    return message.CreateReply($"Hi, {message.From.Name}. Thanks to add me.");
+                }
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ Bot Application1/Dialogs/RootLuisDialog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System.Threading.Tasks;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    [LuisModel("d32e2a29-706d-4665-8bc6-e79cac27a6c3", "1aa58c804a56499c8f706e88158d7704")]
+    public class RootLuisDialog : LuisDialog<object>
+    {
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Sorry, I did not understand '{result.Query}'.");
+            context.Wait(this.MessageReceived);
+        }
+
+        private List<string> startersActions = new List<string>() { "Pick Bulbasaur", "Pick Charmander", "Pick Squirtle" };
+        private List<string> starters = new List<string>() { "Bulbasaur", "Charmander", "Squirtle" };
+
+        [LuisIntent("ShowStarters")]
+        public async Task Starters(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            var message = await activity;
+
+            message.ReplyToId = message.From.Id;
+            message.Text = string.Empty;
+            message.AddHeroCard("Here, take one of these rare pokemon:", startersActions, starters );
+            
+            await context.PostAsync(message);
+        }
+
+        [LuisIntent("PickPokemon")]
+        public async Task PickPokemon(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            var message = await activity;
+
+            int index = startersActions.IndexOf(message.Text);
+            if (index == -1)
+            {
+                //error
+                await context.PostAsync($"You can't choose that pokemon.");
+            }
+            else
+            {
+                await context.PostAsync($"Great! You chose: **{starters[index]}**");
+            }
+        }
+    }
+}
